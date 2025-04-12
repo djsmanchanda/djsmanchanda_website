3969cf452cf91530fdd284549932733b25306578
Divjot Singh Manchanda <68808848+djsmanchanda@users.noreply.github.com>
2025-04-11 19:59:29 -0700

    Bug fix

--- src/app/resources/config.js
+++ src/app/resources/config.js
@@ -16,11 +16,11 @@ const protectedRoutes = {
 
 const style = {
   theme: "dark", // dark | light
-  neutral: "sand", // sand | gray | slate
-  brand: "indigo", // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan
-  accent: "orange", // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan
-  solid: "contrast", // color | contrast
-  solidStyle: "flat", // flat | plastic
+  neutral: "gray", // sand | gray | slate
+  brand: "blue", // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan
+  accent: "cyan", // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan
+  solid: "inverse", // color | contrast
+  solidStyle: "plastic", // flat | plastic
   border: "playful", // rounded | playful | conservative
   surface: "translucent", // filled | translucent
   transition: "all", // all | micro | macro
