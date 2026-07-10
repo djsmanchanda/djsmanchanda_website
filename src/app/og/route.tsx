9ba7c44d601e2a5f79f8569e423c7f6a7bf05b7d
Divjot Singh Manchanda <68808848+djsmanchanda@users.noreply.github.com>
2026-07-11 02:18:24 +0530

    fix(og): reduce edge function bundle size

--- src/app/og/route.tsx
+++ src/app/og/route.tsx
@@ -7,11 +7,6 @@ export const runtime = "edge";
 export async function GET(request: Request) {
   let url = new URL(request.url);
   let title = url.searchParams.get("title") || "Portfolio";
-  const font = fetch(new URL("../../../public/fonts/Inter.ttf", import.meta.url)).then((res) =>
-    res.arrayBuffer(),
-  );
-  const fontData = await font;
-
   return new ImageResponse(
     <div
       style={{
@@ -28,7 +23,7 @@ export async function GET(request: Request) {
           flexDirection: "column",
           justifyContent: "center",
           gap: "4rem",
-          fontFamily: "Inter",
+          fontFamily: "sans-serif",
           fontStyle: "normal",
           color: "white",
         }}
@@ -96,13 +91,6 @@ export async function GET(request: Request) {
     {
       width: 1920,
       height: 1080,
-      fonts: [
-        {
-          name: "Inter",
-          data: fontData,
-          style: "normal",
-        },
-      ],
     },
   );
 }
